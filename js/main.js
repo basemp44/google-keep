656368bc22fdd1e8b6234cea778c3cecb7390cae
Víctor Macías <victor.maciaspalla@soprasteria.com>
2018-04-11 14:58:03 +0200

    style: change main variable name

--- js/main.js
+++ js/main.js
@@ -6,12 +6,12 @@ function headerMenuAsideCallback() {
   let headerMenu = document.getElementById("header-menu");
   headerMenu.addEventListener("click", ev => {
     if (window.innerWidth <= 768) {
-      document.getElementById("aside-content").classList.toggle("navigation-show");
+      document.getElementById("aside-content-container").classList.toggle("navigation-show");
     }
   });
 
   let content = document.getElementById("content");
   content.addEventListener("click", ev => {
-    document.getElementById("aside-content").classList.remove("navigation-show");
+    document.getElementById("aside-content-container").classList.remove("navigation-show");
   })
 }
