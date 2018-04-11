24dac8bf85dd28e55b9577634d41c5227b6bc828
Víctor Macías <victor.maciaspalla@soprasteria.com>
2018-04-11 11:21:27 +0200

    feat: add blur, disabled clicks and callback on content when click on header-menu on mobile

--- js/main.js
+++ js/main.js
@@ -5,6 +5,13 @@ window.onload = function () {
 function headerMenuAsideCallback() {
   let headerMenu = document.getElementById("header-menu");
   headerMenu.addEventListener("click", ev => {
-    document.getElementById("aside-content").classList.toggle("navigation-show");
+    if (window.innerWidth <= 768) {
+      document.getElementById("aside-content").classList.toggle("navigation-show");
+    }
   });
+
+  let content = document.getElementById("content");
+  content.addEventListener("click", ev => {
+    document.getElementById("aside-content").classList.remove("navigation-show");
+  })
 }
